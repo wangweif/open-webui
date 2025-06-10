b9cc0ff68776f4e3437305f0f409b6e3298fe25c
ChengZhikai <buct255@gmail.com>
2025-06-10 11:28:12 +0800

    fix

--- src/lib/components/chat/KnowledgeBaseSelector.svelte
+++ src/lib/components/chat/KnowledgeBaseSelector.svelte
@@ -178,7 +178,7 @@
 														d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012 2v2M7 7h10" />
 												</svg>
 											</div>
-											<div class="truncate">{kb.kb_name}</div>
+											<div class="truncate" title={kb.kb_name}>{kb.kb_name}</div>
 										</div>
 									</div>
 
