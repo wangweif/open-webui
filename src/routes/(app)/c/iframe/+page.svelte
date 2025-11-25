3110d83e5db09eed5e3ecfec6335beb3acdb3f9e
ChengZhikai <buct255@gmail.com>
2025-11-25 16:56:06 +0800

    fix:修改底部栏

--- src/routes/(app)/c/iframe/+page.svelte
+++ src/routes/(app)/c/iframe/+page.svelte
@@ -90,12 +90,15 @@
         </div>
 
         <!-- 底部技术支持信息 -->
-        <div class="w-full py-2 px-4 text-center text-xs text-gray-500 border-t border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 whitespace-pre-wrap">
+        <div class="w-full px-4 text-center text-xs text-gray-500 border-t border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 whitespace-pre-wrap">
             {#if $user?.is_bjny}
                 {FOOTER_TEXT_BJNY || "内容由 AI大模型生成，请仔细甄别。技术支持:北京市农林科学院"}
             {:else}
                 {FOOTER_TEXT || "内容由 AI大模型生成，请仔细甄别。技术支持:北京市农林科学院"}
             {/if}
         </div>
+        <div class="w-full px-4 text-center text-xs dark:border-gray-700 bg-white dark:bg-gray-900 mt-auto whitespace-pre-wrap text-red-500 dark:text-red-400">
+			{"禁止在系统内传输、存储、处理涉密文件和信息"}
+		</div>
     </div>
 </div>
